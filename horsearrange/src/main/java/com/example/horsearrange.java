8323d03dcaad85163ebcd6f19acef1b68000d71f
Jung Verheiden <jung@yuzaco.com>
2019-05-12 00:08:02 -0700

    Fixed Horsearrange algorithm to be more efficient and simple

--- horsearrange/src/main/java/com/example/horsearrange.java
+++ horsearrange/src/main/java/com/example/horsearrange.java
@@ -2,66 +2,57 @@ package com.example;
 
 import java.util.Scanner;
 /* solutions to arrange horses in K  barns to come up with  minimum of whites*blacks
- * in all barns by Jung Verheiden */
+ * in all barns, there has to be at least one horse in all barns.  by Jung Verheiden */
 public class horsearrange
 {
+    String horses;
+    int size;
+    int stables;
     public static void main(String[]  args)
     {
-        String horses = new String("WBWB");
+        String horses = new String("WBWBBBW");
         horsearrange obj = new horsearrange();
         System.out.println( obj.arrange(horses, 2) );
     }
     public int arrange(String a, int b) {
-        int totalHorse = a.length();
-        if ( totalHorse < b )
+        horses = a;
+        size = a.length();
+        stables = b;
+        if ( size < stables )
             return -1;
-        return( (int) kArrange(a, 0, 0,  b) );
+        return( (int) kArrange( 0, 0) );
     }
 
-    long kArrange(String a, int i, int k, int b)
+    long kArrange( int i, int usedStables)
     {
-        int size = a.length();
-        int numStable = b - k;
+        int availStables = stables - usedStables;
         int numHorses = size - i;
+        if ( availStables >  numHorses  )
+            return 0;
         int whites = 0;
         int blacks = 0;
         long min = Integer.MAX_VALUE;
-
-        if ( numStable == numHorses )
+        usedStables++;
+        if ( usedStables > stables )
               return 0;
-        while( (i < size )  )
+        availStables = stables - usedStables;
+        while ( i < ( size - availStables))
         {
-            char c = a.charAt(i);
+            char c =horses.charAt(i);
             i++;
             if ( c == 'W')
             {
                 whites++;
-                if ( blacks == 0 )
-                {
-                    while( ((size-i)  >  numStable) && ( a.charAt(i) == 'W'))
-                    {
-                        i++;
-                        whites++;
-                    }
-                }
             }
-            else
-            {
+            else {
                 blacks++;
-                if ( whites == 0 )
-                {
-                    while(((size-i)  > numStable) && ( a.charAt(i) == 'B'))
-                    {
-                        i++;
-                        blacks++;
-                    }
-                }
             }
-            if (k < ( b -1 ) && ( size - i ) >= (b-1))
-                min   =  Math.min( kArrange(a, i, k+1, b) + whites*blacks, min);
-            else if ( k ==( b -1 ) && ( i ==size ))
-                min = whites*blacks;
+
+            if ( usedStables < stables && (size -i) > (stables - usedStables )) {
+                min = Math.min(kArrange(i, usedStables) + whites * blacks, min);
+            }
         }
+        min = Math.min( min, whites*blacks);
         return min;
     }
 }
